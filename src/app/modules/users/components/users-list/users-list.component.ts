3d3f5edbcb17f4e3fd70894af7a415973fc7606e
Bartosz Grzegorczuk <bartosz.grzegorczuk@gmail.com>
2018-05-03 21:34:24 +0200

    Users container does not refresh users list when response comes...

--- src/app/modules/users/components/users-list/users-list.component.ts
+++ src/app/modules/users/components/users-list/users-list.component.ts
@@ -1,19 +1,23 @@
-import {Component, OnInit, Input, ChangeDetectionStrategy} from '@angular/core';
+import {Component, OnInit, Input, ChangeDetectionStrategy, OnChanges} from '@angular/core';
 import {IUser} from '../../models/user.model';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
   styleUrls: ['./users-list.component.sass'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UsersListComponent implements OnInit {
+export class UsersListComponent implements OnInit, OnChanges {
 
   @Input() users: IUser[];
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngOnChanges(x: any) {
+    console.log('onChanges', x);
   }
+
 
 }
--- src/app/modules/users/containers/users.container.ts
+++ src/app/modules/users/containers/users.container.ts
@@ -11,22 +11,29 @@ import * as userActions from '../store/actions/users.actions';
   selector: 'app-users',
   templateUrl: './users.container.html',
   styleUrls: ['./users.container.sass'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UsersComponent implements OnInit {
 
-  users$: Observable<IUser[]>;
-  users: IUser[];
+  // users$: Observable<IUser[]>;
+  public users: IUser[];
   private page = 1;
   private resultsPerPage = 10;
 
   constructor(private store: Store<IUsersState>) {
-    this.users$ = this.store.select(userSelectors.selectAllUsers); // only initializing users variable as an Observable
+    // this.users$ = this.store.select(userSelectors.selectAllUsers); // only initializing users variable as an Observable
   }
 
   ngOnInit() {
     this.getUsers(this.page, this.resultsPerPage);
-    this.users$.subscribe(users => this.users);
+    this.store.select(userSelectors.selectAllUsers).subscribe(users => {
+      this.users = users;
+      console.log('this.users', this.users);
+    });
+    // this.users$.subscribe(users => {
+    //   this.users = users;
+    //   console.log('this.users', this.users);
+    // });
   }
 
   getUsers(page: number, limit: number) {
