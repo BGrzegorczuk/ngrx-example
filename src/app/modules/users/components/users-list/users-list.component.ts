1c91aa35fc0e560afddac6217d49588444e57803
Bartosz Grzegorczuk <bartosz.grzegorczuk@gmail.com>
2018-05-06 17:39:02 +0200

    Users container does not refresh users list - fixed

--- src/app/modules/users/components/users-list/users-list.component.ts
+++ src/app/modules/users/components/users-list/users-list.component.ts
@@ -1,23 +1,17 @@
-import {Component, OnInit, Input, ChangeDetectionStrategy, OnChanges} from '@angular/core';
+import {Component, OnInit, Input, ChangeDetectionStrategy} from '@angular/core';
 import {IUser} from '../../models/user.model';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
   styleUrls: ['./users-list.component.sass'],
-  // changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UsersListComponent implements OnInit, OnChanges {
+export class UsersListComponent implements OnInit {
 
   @Input() users: IUser[];
 
   constructor() { }
 
   ngOnInit() {}
-
-  ngOnChanges(x: any) {
-    console.log('onChanges', x);
-  }
-
-
 }
--- src/app/modules/users/containers/users.container.ts
+++ src/app/modules/users/containers/users.container.ts
@@ -1,6 +1,5 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {IUser} from '../models/user.model';
-import {Observable} from 'rxjs/Observable';
 import {Store} from '@ngrx/store';
 import {IUsersState} from '../store/reducers';
 import * as userSelectors from '../store/selectors/users.selectors';
@@ -10,30 +9,21 @@ import * as userActions from '../store/actions/users.actions';
 @Component({
   selector: 'app-users',
   templateUrl: './users.container.html',
-  styleUrls: ['./users.container.sass'],
-  // changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./users.container.sass']
 })
 export class UsersComponent implements OnInit {
 
-  // users$: Observable<IUser[]>;
   public users: IUser[];
   private page = 1;
   private resultsPerPage = 10;
 
-  constructor(private store: Store<IUsersState>) {
-    // this.users$ = this.store.select(userSelectors.selectAllUsers); // only initializing users variable as an Observable
-  }
+  constructor(private store: Store<IUsersState>) {}
 
   ngOnInit() {
     this.getUsers(this.page, this.resultsPerPage);
     this.store.select(userSelectors.selectAllUsers).subscribe(users => {
       this.users = users;
-      console.log('this.users', this.users);
     });
-    // this.users$.subscribe(users => {
-    //   this.users = users;
-    //   console.log('this.users', this.users);
-    // });
   }
 
   getUsers(page: number, limit: number) {
